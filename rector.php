3c04492ac82aac861f352e914821e989d427c613
Loïc Faugeron <faugeron.loic@gmail.com>
2026-03-21 15:07:34 +0000

    chore: add Rector

---
+++ rector.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodeQuality\Rector\Assign\CombinedAssignRector;
+use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
+use Rector\Config\RectorConfig;
+use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
+use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
+use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\Visibility\Rector\ClassConst\ChangeConstantVisibilityRector;
+use Rector\Visibility\Rector\ClassMethod\ChangeMethodVisibilityRector;
+
+return RectorConfig::configure()
+    ->withCache(
+        cacheDirectory: '/tmp/rector',
+        cacheClass: FileCacheStorage::class,
+    )
+    ->withPaths([
+        __DIR__,
+    ])
+    ->withSkip([
+        // —— Excluded paths ———————————————————————————————————————————————————
+        // Excluded folders
+        // [dtk]
+        __DIR__.'/config/reference.php',
+        __DIR__.'/var',
+        __DIR__.'/vendor',
+
+        // —— Excluded rules ———————————————————————————————————————————————————
+        // [CODE_QUALITY]
+        CombinedAssignRector::class,
+        // [CODING_STYLE]
+        EncapsedStringsToSprintfRector::class,
+        // [NAMING]
+        RenameParamToMatchTypeRector::class,
+        RenamePropertyToMatchTypeRector::class,
+        RenameVariableToMatchMethodCallReturnTypeRector::class,
+    ])
+    ->withSets([
+        // —— PHP ——————————————————————————————————————————————————————————————
+        SetList::PHP_85,
+
+        // —— Core —————————————————————————————————————————————————————————————
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+        SetList::NAMING,
+        SetList::PRIVATIZATION,
+        SetList::RECTOR_PRESET,
+        SetList::TYPE_DECLARATION,
+        SetList::TYPE_DECLARATION_DOCBLOCKS,
+
+        // —— PHPUnit ——————————————————————————————————————————————————————————
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        PHPUnitSetList::PHPUNIT_120,
+    ])
+    ->withRules([
+        // —— Core —————————————————————————————————————————————————————————————
+        // Inherit parent visibility
+        ChangeConstantVisibilityRector::class,
+        ChangeMethodVisibilityRector::class,
+
+        // Strict Booleans
+        DisallowedEmptyRuleFixerRector::class,
+
+        // More Coding Style
+        ArraySpreadInsteadOfArrayMergeRector::class,
+        StaticClosureRector::class,
+    ]);
