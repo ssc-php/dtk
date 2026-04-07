a1a7d6c60b1db9edff1c6d2db2715ef26c451e56
Loïc Faugeron <faugeron.loic@gmail.com>
2026-04-07 20:25:11 +0200

    feat: integrate Monolog for console warnings

--- config/bundles.php
+++ config/bundles.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\MonologBundle\MonologBundle;
 
 return [
     FrameworkBundle::class => ['all' => true],
+    MonologBundle::class => ['all' => true],
 ];
